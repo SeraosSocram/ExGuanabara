b491de0ee16eeb647c168d0db53e3cbc0d9ed9c4
SeraosSocram <mvcs28@uni9.edu.br>
2022-05-26 11:59:50 -0300

    mais alguns exercicios de python

---
+++ ExGuanabara/ex008.py
@@ -0,0 +1,2 @@
+metros = float(input("Uma distância em metros: "))
+print(f"A medida de {metros}m corresponde a\n{metros/1000}km\n{metros/100}hm\n{metros/10}dam\n{round(metros*10)}dm\n{round(metros*100)}cm\n{round(metros*1000)}mm")
